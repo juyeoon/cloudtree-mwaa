ae3aa551740f2c33ee6946838be53d85c30f4a66
박주연 <18cwceark@gmail.com>
2025-01-19 19:30:14 +0900

    [Upload] L2 & Analysis DAG

---
+++ dags/update_L2_analysis.py
@@ -0,0 +1,39 @@
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.utils.task_group import TaskGroup
+from utils import config as cfg
+from utils import dag_notifications as dn
+from utils import athena_helpers as ath
+from utils import quicksight_helpers as qh
+
+with DAG(
+    dag_id="update_L2_analysis",
+    description="L2 Bucket, Analysis(Redshift, Quicksight) 데이터 갱신",
+    start_date=days_ago(1),
+    schedule_interval=None,
+    catchup=False,
+) as dag:
+
+    start_task = dn.task_alert_dag_state(dag.dag_id, "start")
+
+    agg_database_name = cfg.AGG_DB
+    agg_tables = cfg.AGG_TABLE
+
+    # L2
+    with TaskGroup(group_id="agg_etl") as agg_etl:
+        for table_name in agg_tables:
+            with TaskGroup(group_id=f"agg_etl_{table_name}") as agg_etl:
+                drop_table = ath.drop_table(agg_database_name, table_name)
+                create_table = ath.create_table(agg_database_name, table_name)
+                drop_table >> create_table
+
+    # redshift
+    with TaskGroup(group_id="update_redshift") as update_redshift:
+        pass
+
+    # quicksight
+    refresh_quicksight_spice = qh.task_refresh_spice()
+
+    end_task = dn.task_alert_dag_state(dag.dag_id, "end")
+
+    start_task >> agg_etl >> update_redshift >> refresh_quicksight_spice >> end_task
