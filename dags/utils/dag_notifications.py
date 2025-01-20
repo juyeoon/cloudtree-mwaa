7201d9352d2a1ad7ada90945cf20e3da4fb56868
Park Juyeon <18cwceark@gmail.com>
2025-01-20 11:22:24 +0900

    [Update] task_id 변경 (가독성)

--- dags/utils/dag_notifications.py
+++ dags/utils/dag_notifications.py
@@ -7,7 +7,7 @@ def task_alert_dag_state(dag_id, type):
     task: dag 상태 slack 알림
     """
     return PythonOperator(
-        task_id=f"{dag_id}_{type}_task",
+        task_id=f"{type}_{dag_id}",
         python_callable=sh.send_dag_state_message,
         provide_context=True,
         op_kwargs={
